1b2f403cf69fd26ccd02f2ce3dba4216a0b18fab
KenYonekura <p.p.high.llows@gmail.com>
2020-08-09 16:28:13 +0900

    オークションアプリ完了

--- server/app/Http/Controllers/ItemController.php
+++ server/app/Http/Controllers/ItemController.php
@@ -14,8 +14,33 @@ class ItemController extends Controller
         // モデル名：：Table全件取得
         $items = Item::all();
         // Itemsディレクトリの中のindexページを指定して､itemsの連想配列を代入
-        return view('Items.index',['items' => $items]);
+        return view('Items.index', ['items' => $items]);
     }
+
+    public function create()
+    {
+        return view('items.create');
+    }
+
+    public function store(Request $request)
+    {
+        // インスタンスの作成
+        $item = new Item;
+        // 値の用意
+        $item->name = $request->name;
+        $item->description = $request->description;
+        $item->price = $request->price;
+        $item->seller = $request->seller;
+        $item->email = $request->email;
+        $filename = $request->file('image')->store('public/image');
+        $item->image = basename($filename);
+        $item->timestamps =false;
+        // インスタンスに値を設定して保存
+        $item->save();
+        // 登録したらindexに戻る
+        return redirect('/items');
+    }
+
     // showページへ移動
     public function show($id)
     {
--- server/resources/views/items/index.blade.php
+++ server/resources/views/items/index.blade.php
@@ -1,7 +1,12 @@
 <h1>出品商品一覧</h1>
-<ul>
-    @foreach ($items as $items)
-    <!-- リンク先でidを取得して名前で出力 -->
-    <li><a href="/items/{{ $items->id }}">{{ $items->name }}</a></li>
-    @endforeach
-</ul>
+<table>
+    <tr>
+        @foreach ($items as $item)
+        <!-- リンク先でidを取得して名前で出力 -->
+        <li><a href="/items/{{ $item->id }}">{{ $item->name }}</a></li>
+        @endforeach
+    </tr>
+</table>
+
+<!-- 新規登録画面へジャンプする -->
+<a href="/items/create">出品する</a>
